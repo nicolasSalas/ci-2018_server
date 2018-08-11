548a08f68082d85a760efdf12a078e1f3eb86ed4
nicolasSalas <npsa1794@gmail.com>
2018-08-11 12:45:05 -0400

    subiendo contacto icorp

---
+++ Contacto/contact.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+require_once 'libs/phpmailer/PHPMailerAutoload.php';
+
+$errors =[];
+
+if(isset($_POST['name'],$_POST['email'],$_POST['message'])){
+    $fields=[
+        'name'=>$_POST['name'],
+        'email'=>$_POST['email'],
+        'message'=>$_POST['message']
+    ];
+    foreach($fields as $field=>$data){
+        if(empty($data)){
+            $errors[]='The '.$field . ' field is required.';
+        }
+    }
+    if(empty($errors)){
+        $m=new PHPMailer;
+        $m->isSMTP();
+        $m->SMTPAuth=true;
+        $m->Host='smtp.gmail.com';
+        $m->Username='npsa1794@gmail.com';//replace with your email address
+        $m->Password='npsa1794.';//replace with your password
+        $m->SMTPSecure='ssl';
+        $m->Port=465;
+
+        $m->isHTML();
+        $m->Subject ='Contact form Submitted';
+        $m->Body='From:'.$fields['name'].'('.$fields['email'].')<p>'.$fields['message'].'</p>';
+
+        $m->FromName='Contact';
+        $m->AddAddress('npsa1794@gmail.com','npsa1794.');
+        if ($m->send()) {
+            header('Location:thanks.php');
+            die();
+        }else{
+            $errors[]="Sorry ,Could not send email.Try again later.";
+        }
+    }
+}else{
+    $errors[]= 'Something went wrong';
+}
+$_SESSION['errors']=$errors;
+$_SESSION['fields']=$fields;
+header ('Location:index.php');
---
+++ Contacto/thanks.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Corporate index</title>
+
+  <!-- Bootstrap -->
+  <link rel="shortcut icon" type="image/png" href="favicon.ico" />
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link href="css/animate.min.css" rel="stylesheet">
+  <link href="css/animate.css" rel="stylesheet" />
+  <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <!-- Navigation -->
+  <div id="navigation">
+    <nav class="navbar navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="row">
+          <div class="site-logo">
+            <a href="index.html" class="brand">
+              <img src="./img/logo.png" width="50%" alt="logo">
+            </a>
+          </div>
+          <!-- /.Navbar-collapse -->
+        </div>
+      </div>
+      <!-- /.container -->
+    </nav>
+  </div>
+  <!-- /Navigation -->
+
+  <div id="home">
+    <div class="slider">
+      <img src="img/3.jpg" class="img-responsive" />
+    </div>
+  </div>
+
+<section id="proposito" style='padding-bottom: 100px;'> 
+    <div class="container marginbot-50">
+     <div class="row">
+       <div class="col-lg-8 col-lg-offset-2">
+         <div class="wow flipInY" data-wow-offset="0" data-wow-delay="0.4s">
+           <div class="section-heading text-center">
+             <h2 class="h-bold">Recibido!</h2>
+             <div class="divider-header"></div>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div> 
+
+    <div class="container"> 
+      <div class="row">
+     <!--   <div class="col-md-6">
+         <img src="img/dummy1.jpg" alt="" class="img-responsive" />
+       </div>  -->
+      <div class="col-md-12" style="display: flex;
+      justify-content: center;">
+       <p>
+        Gracias por contactarse con nostors.
+        Le responderemos a la brevedad
+       </p>
+      </div>
+     </div>
+    </div>
+ </section>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-md-offset-3">
+            <div class="text-center">
+              <a href="#home" class="scrollup">
+                <i class="fa fa-angle-up fa-3x"></i>
+              </a>
+              <p>
+                <img src="img/favicon.ico" height="42" width="42"> &copy;Indice Empresarial Corporativo,todos los derechos reservados
+              </p>
+              <div class="credits">
+                  <p>
+                    Para contactarse con nosotros escríbanos
+                  </p>
+                  <p>
+                     a <u><big>contacto@corporateindex.cl </big>  </u> 
+                  </p>
+                  <p>
+                    Teléfono de contacto <u><big>+56988264551 </big> </u> 
+                  </p>
+                 <!--  <p>
+                      Teléfono de contacto +56988264551
+                  </p> -->
+                     
+                Designed by
+                <big>
+                  <u>
+                    <a href="">JeraInsigth</a>
+                  </u>
+                </big>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Core JavaScript Files -->
+    <script src="js/jquery-2.1.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <!-- <script src="https://maps.googleapis.com/maps/api/js?key="></script> -->
+    <script src="js/functions.js"></script>
+    <script src="contactform/contactform.js"></script>
+
+</body>
+
+</html>
