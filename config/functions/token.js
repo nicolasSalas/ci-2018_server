e2fbae662fcfe53f9b476e3dd90274fd73b0d286
nicolasSalas <npsa1794@gmail.com>
2018-08-29 18:49:47 -0300

    cambio de la duracion del link a 1 dia

--- config/functions/token.js
+++ config/functions/token.js
@@ -10,7 +10,7 @@ module.exports = {
         const payload = {
             sub: data.token,
             iat: moment().unix(),
-            exp: moment().add(5, 'minutes').unix(),
+            exp: moment().add(1, 'days').unix(),
         }
         return jwt.encode(payload, config.SECRET_TOKEN_SERVER)
     },
