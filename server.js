b0ef6096f0ed73cf02f67deffe6306c597887a4f
nicolasSalas <npsa1794@gmail.com>
2018-09-26 22:44:42 -0300

    test de select

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
-        "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
+        "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+        //"Origin, X-Requested-With, Content-Type, Accept,Access-Control-Allow-Headers, Authorization"
     );
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     next();
 });
 
