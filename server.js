02d784284842daa1231893f243f6d0edb5db6ae7
nicolasSalas <npsa1794@gmail.com>
2018-11-19 23:35:53 -0300

    subiendo server

--- server.js
+++ server.js
@@ -22,56 +22,6 @@ app.get('/', function (request, resp, next) {
     resp.render('index')
 });
 
-<<<<<<< HEAD
-var Insert_AnswerToQuestion = require('./Server/AnswerToQuestion/_Insert');
-var Insert_Contact = require('./Server/Contact/_Insert');
-var Insert_EnterpriseEvaluation = require('./Server/EnterpriseEvaluation/_Insert');
-var Insert_EnterpriseSelected = require('./Server/EnterpriseSelected/_Insert');
-var Insert_EnterpriseStored = require('./Server/EnterpriseStored/_Insert');
-var Insert_Factor = require('./Server/Factor/_Insert');
-var Insert_Link = require('./Server/Link/_Insert');
-var Insert_MailsSurveyed = require('./Server/MailsSurveyed/_Insert');
-var Insert_Question = require('./Server/Question/_Insert');
-var Insert_RelationShip = require('./Server/RelationShip/_Insert');
-var Insert_Sector = require('./Server/Sector/_Insert');
-var Insert_Size = require('./Server/Size/_Insert');
-var Insert_Subsector = require('./Server/Subsector/_Insert');
-var Insert_Surveyed = require('./Server/Surveyed/_Insert');
-var Insert_Variables = require('./Server/Variables/_Insert');
-var Insert_VariablesSelected = require('./Server/VariablesSelected/_Insert');
-var GetEnterprise_Stored = require('./Server/EnterpriseStored/_ReadWhere');
-
-//
-var Read_Sector = require('./Server/Sector/_Read');
-var Read_Link = require('./Server/Link/_Read');
-var GetSectorId_from_enterprise_evaluation = require('./Server/EnterpriseEvaluation/_ReadWhere');
-var GetSubsector_and_id = require('./Server/Subsector/_ReadWhere');
-var GetMailSurveyed_And_ID = require('./Server/MailsSurveyed/_ReadWhere')
-app.use('/webservices',
-    Insert_AnswerToQuestion,
-    Insert_Contact,
-    Insert_EnterpriseEvaluation,
-    Insert_EnterpriseSelected,
-    Insert_EnterpriseStored,
-    Insert_Factor,
-    Insert_Link,
-    Insert_MailsSurveyed,
-    Insert_Question,
-    Insert_RelationShip,
-    Insert_Sector,
-    Insert_Size,
-    Insert_Subsector,
-    Insert_Surveyed,
-    Insert_Variables,
-    Insert_VariablesSelected,
-    GetEnterprise_Stored,
-    Read_Sector,
-    Read_Link,
-    GetSectorId_from_enterprise_evaluation,
-    GetSubsector_and_id,
-    GetMailSurveyed_And_ID
-
-=======
 var Insert_AnswerToQuestion = require("./Server/AnswerToQuestion/_Insert");
 var Insert_Contact = require("./Server/Contact/_Insert");
 var Insert_EnterpriseEvaluation = require("./Server/EnterpriseEvaluation/_Insert");
@@ -128,7 +78,6 @@ app.use(
   GetMailSurveyed_And_ID,
   Read_GetEnterpriseEvaluation,
   Read_View
->>>>>>> d92dc7cfc3d06c725566087c7e39d60d001b538a
 );
 
 app.listen(8081, function () {
